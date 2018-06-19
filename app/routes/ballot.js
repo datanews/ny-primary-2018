925bc83d654e166435b976bdefc0c69d022c53e7
Brian Whitton <me@brianwhitton.com>
2018-06-19 18:18:11 -0400

    add initial ballot route template

--- app/routes/ballot.js
+++ app/routes/ballot.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('update', this.update);
+  },
+
+  update(party, district) {
+
+  }
 });
