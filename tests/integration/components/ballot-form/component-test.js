e33059805d8456de56998d37377db2ecb62a11f8
Brian Whitton <me@brianwhitton.com>
2018-06-19 17:37:13 -0400

    more ballot form tests

--- tests/integration/components/ballot-form/component-test.js
+++ tests/integration/components/ballot-form/component-test.js
@@ -1,11 +1,21 @@
 import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find, click, findAll, triggerKeyEvent, fillIn } from '@ember/test-helpers';
+import {
+  render,
+  find,
+  click,
+  findAll,
+  triggerKeyEvent,
+  fillIn,
+} from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import test from 'ember-sinon-qunit/test-support/test';
 import Service from '@ember/service';
 import { task } from 'ember-concurrency';
 import sinon from 'sinon';
+
+import ERRORS from 'ny-primary-2018/lib/errors';
+import { MULTIPLE_RESULTS } from '../../../fixtures/geocoder';
 
 let findSpy;
 let district;
@@ -50,12 +60,12 @@ module('Integration | Component | ballot-form', function(hooks) {
     await render(hbs`{{ballot-form}}`);
 
     await click('.ballot-form__submit');
-    await triggerKeyEvent('.ballot-form__input', 'keypress', 'Enter');
+    await triggerKeyEvent('.ballot-form__field', 'keypress', 'Enter');
 
     assert.equal(findSpy.callCount, 0);
 
-    await fillIn('.ballot-form__input', '123 main street')
-    await triggerKeyEvent('.ballot-form__input', 'keypress', 'Enter');
+    await fillIn('.ballot-form__field', '123 main street')
+    await triggerKeyEvent('.ballot-form__field', 'keypress', 'Enter');
     await click('.ballot-form__submit');
 
     assert.equal(findSpy.callCount, 0);
@@ -65,17 +75,32 @@ module('Integration | Component | ballot-form', function(hooks) {
     assert.equal(findSpy.callCount, 1);
   });
 
-  test('submitting calls onSubmit with district and party', async function(assert) {
+  test('submitting calls showBallot with district and party', async function(assert) {
     district = 1;
     const PARTY = 'dem';
     let spy = this.spy();
-    this.set('onSubmit', spy);
+    this.set('showBallot', spy);
 
-    await render(hbs`{{ballot-form onSubmit=onSubmit}}`);
-    await fillIn('.ballot-form__input', '123 main street')
+    await render(hbs`{{ballot-form showBallot=showBallot}}`);
+    await fillIn('.ballot-form__field', '123 main street')
     await click('[data-test-selector=dem]');
 
     await click('.ballot-form__submit');
     assert.ok(spy.calledWith(district, PARTY));
+  });
+
+  test('it shows suggested addresses', async function(assert) {
+    this.set('error', ERRORS.MULTIPLE_LOCATIONS(MULTIPLE_RESULTS).error);
+    await render(hbs`{{ballot-form error=error}}`);
+
+    assert.equal(findAll('.ballot-form__suggestion').length, MULTIPLE_RESULTS.length);
+  });
+
+  test('it renders an error message', async function(assert) {
+    let error = ERRORS.NO_RESULTS().error;
+    this.set('error', error);
+    await render(hbs`{{ballot-form error=error}}`);
+
+    assert.equal(find('.ballot-form__error').textContent.trim(), error.message);
   });
 });
