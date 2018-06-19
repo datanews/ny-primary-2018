4d29dd27de9a817ad006b45f917fc74a41e661a6
Brian Whitton <me@brianwhitton.com>
2018-06-19 08:38:14 -0400

    gzip guide.json

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const fs = require('fs');
+const zlib = require('zlib');
 
 const aws = require('aws-sdk');
 const getJSON = require('./lib/get-json.js');
@@ -11,12 +12,14 @@ const s3 = new aws.S3();
 
 getJSON().then(json => {
   if (['prod', 'staging'].includes(ENV)) {
+    let Body = zlib.deflateSync(Buffer.from(JSON.stringify(json)));
     s3.putObject({
-      Body: Buffer.from(JSON.stringify(json)),
+      Body,
       Bucket: process.env.AWS_BUCKET,
       Key: `${process.env.AWS_PREFIX}/data/guide.json`,
       ACL: 'public-read',
       ContentType: 'application/json',
+      ContentEncoding: 'gzip',
       Expires: 0
     }).promise()
     .then(() => console.log('success')); // eslint-disable-line
