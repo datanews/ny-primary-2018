b39b2dc9824e925c63f36633579e5041049991ff
Brian Whitton <me@brianwhitton.com>
2018-06-19 17:29:37 -0400

    adds a type arg and remove promise rejection from signature
    make it more usable by other contexts

--- app/lib/errors.js
+++ app/lib/errors.js
@@ -1,17 +1,24 @@
-const error = (message, ops) => ({error: { message, ...ops}});
+const ERRORS = {
+  NO_RESULTS: 'NO_RESULTS',
+  OUT_OF_BOUNDS: 'OUT_OF_BOUNDS',
+  BAD_STATUS: 'BAD_STATUS',
+  MULTIPLE_LOCATIONS: 'MULTIPLE_LOCATIONS'
+};
+
+const error = (message, type, ops) => ({error: { message, type, ...ops}});
 
 export default {
-  NO_RESULTS: reject => reject(error('Could not find address. Please try another.')),
-  OUT_OF_BOUNDS: reject => reject(error('Please enter a New York City address.')),
-  BAD_STATUS: (reject, status) => reject(error(`Could not look up address because ${status}`)),
-  MULTIPLE_LOCATIONS(reject, results) {
-    reject(error('Did you mean...', {
+  [ERRORS.NO_RESULTS]: () => error('Could not find address. Please try another.', ERRORS.NO_RESULTS),
+  [ERRORS.OUT_OF_BOUNDS]: () => error('Please enter a New York City address.', ERRORS.OUT_OF_BOUNDS),
+  [ERRORS.BAD_STATUS]: status => error(`Could not look up address because ${status}`, ERRORS.BAD_STATUS),
+  [ERRORS.MULTIPLE_LOCATIONS](results) {
+    return error('Did you mean...', ERRORS.MULTIPLE_LOCATIONS, {
       locations: results.map(({ formatted_address, geometry }) => {
         return {
           address: formatted_address,
           ...geometry.location.toJSON()
         }
       })
-    }));
+    });
   }
 }
