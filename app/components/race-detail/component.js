e7d0dc0dac8a52c74bedaefd9c07160e74fd70dd
Brian Whitton <me@brianwhitton.com>
2018-06-20 17:48:54 -0400

    layout and UI fixes

--- app/components/race-detail/component.js
+++ app/components/race-detail/component.js
@@ -4,10 +4,15 @@ import { partyName } from '../../helpers/party-name';
 
 export default Component.extend({
   classNames: ['race-detail'],
+  classNameBindings: ['empty:is-empty'],
+
+  empty: computed('race', 'candidates', function() {
+    return !this.race || !this.candidates.length;
+  }),
 
   candidates: computed('race.candidates', 'party', function() {
     if (!this.race) {
-      return;
+      return [];
     }
     let { candidates } = this.race;
     return candidates.filterBy('party', partyName([ this.party, 'formal']));
