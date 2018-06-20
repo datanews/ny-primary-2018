6e7728e2f6a751698bd2eaafcfd6c196940accf6
Brian Whitton <me@brianwhitton.com>
2018-06-20 13:26:29 -0400

    test that race detail filters based on party

--- tests/integration/components/race-detail/component-test.js
+++ tests/integration/components/race-detail/component-test.js
@@ -15,25 +15,34 @@ module('Integration | Component | race-detail', function(hooks) {
         firstName: 'Bar',
         surname: 'Baz',
         incumbent: 'yes',
-        website: 'www.foo.com'
+        website: 'www.foo.com',
+        party: 'Democrat'
       }, {
         firstName: 'Buz',
         surname: 'Fuz',
         incumbent: 'no',
-        website: 'www.wizzz.com'
+        website: 'www.wizzz.com',
+        party: 'Democrat'
+      }, {
+        firstName: 'Wiz',
+        surname: 'Wqz',
+        incumbent: 'yes',
+        website: 'www.wizzz.com',
+        party: 'Republican'
       }]
     };
     this.set('race', RACE);
-    await render(hbs`{{race-detail race=race}}`);
+    await render(hbs`{{race-detail race=race party='dem'}}`);
 
-    assert.equal(findAll('.race-candidate__item').length, 2);
+    assert.equal(findAll('.race-candidate__item').length, 2, 'should only show democrat candidates');
     assert.equal(find('.race-summary').textContent.trim(), RACE.nutshell);
     assert.equal(find('.race-title').textContent.trim(), `District ${RACE.district}`);
+
+    await render(hbs`{{race-detail race=race party='rep'}}`);
+    assert.equal(findAll('.race-candidate__item').length, 1, 'should only show republican candidates');
+
+    await render(hbs`{{race-detail race=race party='green'}}`);
+
+    assert.equal(find('.race-detail').textContent.trim(), 'No Green Candidates Available');
   });
-
-  test('empty state', async function(assert) {
-    await render(hbs`{{race-detail party='dem'}}`);
-
-    assert.equal(find('.race-detail').textContent.trim(), 'No Democratic Candidates Available');
-  })
 });
