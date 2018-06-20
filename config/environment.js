4d42b5dfc9bdc14532c57f74ec8045b01f7f07a1
Brian Whitton <me@brianwhitton.com>
2018-06-20 19:21:28 -0400

    set location type to hash

--- config/environment.js
+++ config/environment.js
@@ -52,6 +52,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.routerRoot = `/${process.env.AWS_PREFIX}/`;
+    ENV.locationType = 'hash';
   }
 
   return ENV;
