de4b642d4d78c8d3cb0279c19742fed34b81d5ed
Brian Whitton <me@brianwhitton.com>
2018-06-20 10:27:55 -0400

    update test to assert context control

--- tests/integration/components/ballot-switcher/component-test.js
+++ tests/integration/components/ballot-switcher/component-test.js
@@ -1,26 +1,49 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { selectChoose } from 'ember-power-select/test-support';
 
 module('Integration | Component | ballot-switcher', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('usage', async function(assert) {
+    this.setProperties({
+      party: 'dem',
+      districts: [{id: 1}, {id: 2}],
+      selected: [{id: 1}],
+      update: (party, district) => assert.deepEqual([party, district], ['rep', {id: 2}], 'update is called with current party and district')
+    });
 
-    await render(hbs`{{ballot-switcher}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{#ballot-switcher}}
-        template block text
+      {{#ballot-switcher as |switcher|}}
+        {{switcher.dropdown
+          districts=districts
+          selected=selected
+          onChange=(action update party)
+        }}
+
+        {{#switcher.parties onClick=(action (mut party)) party=party as |parties|}}
+        <ul>
+          <li>
+            {{parties.button 'dem'}}
+          </li>
+          <li>
+            {{parties.button 'rep'}}
+          </li>
+          <li>
+            {{parties.button 'green'}}
+          </li>
+          <li>
+            {{parties.button 'ind'}}
+          </li>
+        </ul>
+        {{/switcher.parties}}
+
       {{/ballot-switcher}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await click('.ballot-parties li:nth-child(2) button');
+    await selectChoose('.ballot-switcher__dropdown', 'District 2');
   });
 });
