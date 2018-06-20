c4ec9e593170291258b3bf166aec03e9aa5bb679
Brian Whitton <me@brianwhitton.com>
2018-06-20 13:29:47 -0400

    ballot route logic

--- app/routes/ballot.js
+++ app/routes/ballot.js
@@ -1,12 +1,28 @@
 import Route from '@ember/routing/route';
+import { bind } from '@ember/runloop';
 
 export default Route.extend({
+  model({ district, party }) {
+    let race = this.store.peekRecord('race', district);
+    return {
+      race,
+      party,
+      selected: race ? race : {id: district},
+      districts: this.store.peekAll('race'),
+    };
+  },
   setupController(controller, model) {
     this._super(controller, model);
-    controller.set('update', this.update);
+    controller.set('update', bind(this, 'update'));
   },
 
-  update(party, district) {
-
+  update(type, value) {
+    let { district, party } = this.paramsFor(this.routeName);
+    if (type === 'district') {
+      district = value.get('id');
+    } else if (type === 'party') {
+      party = value;
+    }
+    this.transitionTo('ballot', district, party);
   }
 });
