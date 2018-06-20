81728d97bd7d0b8d54287de36e206bbb5aba9985
Brian Whitton <me@brianwhitton.com>
2018-06-20 15:06:31 -0400

    scroll up on nav

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,11 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.findAll('race');
+  },
+
+  actions: {
+    didTransition() {
+      window.scrollTo(0, 0);
+    }
   }
 });
