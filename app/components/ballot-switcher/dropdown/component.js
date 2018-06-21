08bde4572ebc4e3ddacb8b715749d3d2780b5097
Brian Whitton <me@brianwhitton.com>
2018-06-21 15:26:15 -0400

    insert chosen district into dropdown if it doesn't have any races

--- app/components/ballot-switcher/dropdown/component.js
+++ app/components/ballot-switcher/dropdown/component.js
@@ -1,10 +1,19 @@
 import Component from '@ember/component';
 import { reads } from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   classNames: ['ballot-switcher__dropdown'],
 
   chosen: reads('selected'),
+  options: computed('districts', 'chosen', function() {
+    let ids = this.districts.mapBy('id');
+    if (!ids.includes(this.chosen)) {
+      ids.push(this.chosen);
+      ids.sort((a, b) => Number(a) - Number(b));
+    }
+    return ids;
+  }),
 
   changeHandler(e) {
     this.set('chosen', e.target.value);
