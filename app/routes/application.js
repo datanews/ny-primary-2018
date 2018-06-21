7f229435958892fc1e4291e296a7dcc19f3ec0b5
Brian Whitton <me@brianwhitton.com>
2018-06-21 11:39:42 -0400

    set up poller

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,12 @@
 import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
+import pym from 'pym';
 
 export default Route.extend({
   locator: inject('district-locator'),
 
   beforeModel() {
+    new pym.Child({polling: 200});
     this.locator.loadDistricts.perform();
   },
   model() {
