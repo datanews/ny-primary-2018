302b8ffb888a5bcb1afdc2e13d089139036ce659
Michael Hearn <git@mikehearn.net>
2018-09-05 18:50:26 -0400

    Add type to race model

--- app/models/race.js
+++ app/models/race.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   district:     DS.attr('string'),
   party:        DS.attr('string'),
+  type:         DS.attr('string'),
   wnycHeadline: DS.attr('string'),
   wnycLink:     DS.attr('string'),
   ggHeadline:   DS.attr('string'),
