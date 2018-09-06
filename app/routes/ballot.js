52ec00df2a51c08072cfd1765a0ebd40fad537f5
Michael Hearn <git@mikehearn.net>
2018-09-06 17:47:36 -0400

    Add switch to show no-district-election warning at bottom

--- app/routes/ballot.js
+++ app/routes/ballot.js
@@ -24,6 +24,7 @@ export default Route.extend({
     return {
       races,
       allDistrictRaces,
+      hasDistrictRaces: (allDistrictRaces.mapBy('id').indexOf(district) > -1),
       party,
       selected: district,
       districts: this.store.peekAll("race")
